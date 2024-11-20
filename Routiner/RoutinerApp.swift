b521fc9aeadbbe067a0392066494ea823397fdc7
Yaroslav Orlov <ulverforest@gmail.com>
2024-11-20 12:23:41 +0200

    Initial Commit

---
+++ Routiner/RoutinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutinerApp.swift
+//  Routiner
+//
+//  Created by Yaroslav Orlov on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RoutinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
