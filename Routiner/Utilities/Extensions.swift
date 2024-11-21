56961cb044a00784efbec173c7701abffb0ba29b
Yaroslav Orlov <ulverforest@gmail.com>
2024-11-21 14:32:37 +0200

    add colors

--- Routiner/Utilities/Extensions.swift
+++ Routiner/Utilities/Extensions.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 extension Color {
     static let backgroundGradient = LinearGradient(colors: [Color("gradientPurple"), Color("gradientBlue")], startPoint: .topLeading, endPoint: .bottomTrailing)
+    
+    static let primaryBlue10 = Color("primaryBlue10")
     static let primaryBlue20 = Color("primaryBlue20")
     static let primaryBlue40 = Color("primaryBlue40")
     static let primaryBlue60 = Color("primaryBlue60")
+    static let primaryBlue80 = Color("primaryBlue80")
+    static let primaryBlue100 = Color("primaryBlue100")
+    
+    static let primaryBlack10 = Color("primaryBlack10")
+    static let primaryBlack20 = Color("primaryBlack20")
+    static let primaryBlack40 = Color("primaryBlack40")
+    static let primaryBlack80 = Color("primaryBlack80")
 }
